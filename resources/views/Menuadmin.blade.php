748b15b2f33cb3b8e96232ad79621222fcc92c73
boryspine <119823559+boryspine@users.noreply.github.com>
2024-06-27 21:26:17 -0600

    Update Menuadmin.blade.php
    Agregando Favicon

--- resources/views/Menuadmin.blade.php
+++ resources/views/Menuadmin.blade.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
     <title>MenuAdmin</title>
+     <link rel="icon" href="ManageUsers.png" type="">
 </head>
 <body>
     
