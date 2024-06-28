9191c51673fa895db68937aaf3b7c6fea7554d94
boryspine <119823559+boryspine@users.noreply.github.com>
2024-06-27 21:26:39 -0600

    Update Menumoderador.blade.php
    Agregando Favicon

--- resources/views/Menumoderador.blade.php
+++ resources/views/Menumoderador.blade.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
     <title>MenuModerador</title>
+     <link rel="icon" href="ManageUsers.png" type="">
 </head>
 <body>
     
